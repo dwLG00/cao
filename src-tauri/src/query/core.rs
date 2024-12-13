30fa3039532e5edc932d3d05af1df68ef99fdad7
dwLG00 <wallace.dylan00@gmail.com>
2024-12-13 15:42:53 -0600

    sql query generation

--- src-tauri/src/query/core.rs
+++ src-tauri/src/query/core.rs
@@ -1,6 +1,8 @@
 use serde::{Serialize, Deserialize};
 use std::{cmp::Ordering, default::Default};
 use regex::Regex;
+use sqlx::sqlite::SqlitePool;
+use sqlx::{query_builder::QueryBuilder, Execute};
 
 use chrono::{Utc, DateTime};
 
@@ -8,7 +10,7 @@ use anyhow::Result;
 
 use super::super::tasks::core::TaskDescription;
 
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq)]
 pub enum Availability {
     All,
     #[default]
@@ -62,6 +64,90 @@ impl BrowseRequest {
             backup_a.timestamp_millis().cmp(&backup_b.timestamp_millis())
        }
     }
+
+    pub fn search_query(&self) -> String {
+        let today = Utc::now();
+
+        let mut query = {
+            let mut query = QueryBuilder::new("SELECT * FROM tasks ");
+            if self.availability != Availability::All || self.tags.len() != 0 {
+                query.push(" WHERE ");
+            }
+            match self.availability {
+                Availability::Incomplete => {
+                    query.push(" complete == 0 ");
+                },
+                Availability::Available => {
+                    query.push(" complete == 0 AND (start == NULL OR start < ?) ");
+                    query.push_bind(today);
+                },
+                Availability::Done => {
+                    query.push(" complete == 1 ");
+                },
+                Availability::All => {
+                    query.push(" TRUE ");
+                }
+            }
+            for tag in self.tags.iter() {
+                query.push(" AND tag LIKE '%?%' ");
+                query.push_bind(tag);
+            }
+            query
+        };
+        let order_direction = if self.order.ascending { "ASC" } else { "DESC" };
+        match self.order.order {
+            OrderType::Captured => {
+                query.push(" ORDER BY captured ");
+                query.push_bind(order_direction);
+            },
+            OrderType::Start => {
+                query.push(" ORDER BY start ");
+                query.push_bind(order_direction);
+                query.push(" , captured ");
+                query.push_bind(order_direction);
+            },
+            OrderType::Due => {
+                query.push(" ORDER BY due ");
+                query.push_bind(order_direction);
+                query.push(" , captured ");
+                query.push_bind(order_direction);
+            },
+            OrderType::Scheduled => {
+                query.push(" ORDER BY schedule ");
+                query.push_bind(order_direction);
+                query.push(" , captured ");
+                query.push_bind(order_direction);
+            }
+        }
+        query.build().sql().into()
+    }
+
+    /// Query directly from the sqlite file
+    /*
+    pub fn execute_sqlite(&self, pool: &SqlitePool) -> Result<Vec<>>{
+        // Filter by...
+        // -> does it have the right availability?
+        // -> does it contain the right tags?
+        // -> does it match the regex? <== impl later
+        // Sort by either
+        // -> captured time
+        // -> start time
+        // -> due time
+        // -> scheduled time
+
+        let today = Utc::now();
+
+        let availability_str = match self.availability {
+            Availability::Incomplete => "completed = 0",
+            Availability::Available => "completed = 0 AND (start = NULL OR start < )",
+        };
+
+        let mut filtered:Vec<_> = {
+            // Filter by availability and tags
+            let filtered_availability: Vec<_> = sqlx::query_as("SELECT * FROM tables WHERE ")
+        }
+    }
+    */
 
     /// Use a QueryRequest to filter a list of tasks
     pub fn execute<'a>(&self, data: &'a[TaskDescription]) -> Result<Vec<&'a TaskDescription>>{
